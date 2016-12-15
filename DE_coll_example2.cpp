8cebe0988b3ee2345d981d66f1046770414e6666
Tomoaki Okayama <okayamat@gmail.com>
2016-12-15 15:45:40 +0900

    Added solvers by means of DE-Sinc-collocation method.

---
+++ DE_coll_example2.cpp
@@ -0,0 +1,170 @@
+#include <cpplapack.h>
+#include <gsl/gsl_sf_expint.h>
+#include <DE_trans.h>
+#include <time.h>
+
+double ex1_u(double x)
+{
+  return sqrt(1 + x*x) * sinh(atan(x));
+}
+
+double ex2_u(double x)
+{
+  return (cosh(atan(x)) + x*sinh(atan(x)))/sqrt(1 + x*x);
+}
+
+double S(int j, double h, double x)
+{
+  double val = M_PI*(x - j*h)/h;
+  if (val == 0)
+    return 1.0;
+  else
+    return sin(val)/val;
+}
+
+double J(int j, double h, double x)
+{
+  return h * (0.5 + M_1_PI * gsl_sf_Si(M_PI * (x / h - j)));
+}
+
+double xDE(double a, double b, int k, double h)
+{
+  return DE_trans(a, b, k*h);
+}
+
+double del0(int i, int j)
+{
+  if (i == j)
+    return 1.0;
+  else
+    return 0.0;
+}
+
+double del1(int i, int j)
+{
+  return 0.5 + M_1_PI * gsl_sf_Si(M_PI * (i - j));
+}
+
+double wa(double a, double b, double x)
+{
+  return (b-x)/(b-a);
+}
+
+double wb(double a, double b, double x)
+{
+  return (x-a)/(b-a);
+}
+
+double waDE(double x)
+{
+  return DE_trans(1, 0, x);
+}
+
+double wbDE(double x)
+{
+  return DE_trans(0, 1, x);
+}
+
+int main()
+{
+  double a = 0.0;
+  double b = 1.0;
+  double d = 0.8332;
+  double tmp;
+  clock_t start, end;
+  double time;
+
+  for (int N = 1; N <= 70; N += 8) {
+  start = clock();
+
+  int m = (2*N+1);
+  int n = (2*N+1)*2;
+  double h = log(2 * d * N) / N;
+
+  CPPL::dgematrix A_N(n,n);
+  CPPL::dcovector f_N(n);
+  CPPL::dcovector y_1(m);
+  CPPL::dcovector y_2(m);
+
+  for (int i = 0; i < m; i++) {
+    for (int j = 0; j < m; j++) {
+      A_N(i, j)     = del0(i, j);
+      A_N(m+i, m+j) = del0(m+i, m+j);
+    }
+  }
+
+  for (int i = -N; i <= N; i++) {
+    for (int j = -N; j <= N; j++) {
+      tmp = DE_trans(a, b, j*h);
+      A_N(i+N+m, j+N)   = - 2 * h * DE_trans_div(a, b, j*h) * del1(i, j) / ((1+tmp*tmp)*(1+tmp*tmp));
+      A_N(i+N  , j+N+m) = - h * DE_trans_div(a, b, j*h) * del1(i, j);
+    }
+  }
+
+  for (int i = 0; i < m; i++) {
+    f_N(i)   = 0;
+    f_N(i+m) = 1;
+  }
+
+  A_N.dgesv(f_N);
+
+  for (int i = 0; i < m; i++) {
+    y_1(i) = f_N(i);
+    y_2(i) = f_N(i+m);
+  }
+
+  int SAMPLE = 1000;
+  double hh = (b-a)/SAMPLE;
+  double maxerr = 0;
+
+  for (int i=1; i< SAMPLE; i++) {
+    double err;
+    double ans = 0;
+    double x = i*hh;
+
+    for (int j=N; j>0; j--) {
+      ans += (y_1( j+N) - y_1(-N+N)*waDE( j*h) - y_1( N+N)*wbDE( j*h))
+                       * S( j, h, DE_trans_inv(a, b, x));
+      ans += (y_1(-j+N) - y_1(-N+N)*waDE(-j*h) - y_1( N+N)*wbDE(-j*h))
+                       * S(-j, h, DE_trans_inv(a, b, x));
+    }
+      ans += (y_1( 0+N) - y_1(-N+N)*waDE(0*h) - y_1( N+N)*wbDE(0*h))
+                       * S( 0, h, DE_trans_inv(a, b, x));
+      ans += y_1(-N+N)*wa(a, b, x) + y_1( N+N)*wb(a, b, x);
+
+    err = fabs(ex1_u(x) - ans);
+    //    std::cout << x << "\t" << err << std::endl;
+
+    if (maxerr < err)
+      maxerr = err;
+  }
+
+  for (int i=1; i< SAMPLE; i++) {
+    double err;
+    double ans = 0;
+    double x = i*hh;
+
+    for (int j=N; j>0; j--) {
+      ans += (y_2( j+N) - y_2(-N+N)*waDE( j*h) - y_2( N+N)*wbDE( j*h))
+                       * S( j, h, DE_trans_inv(a, b, x));
+      ans += (y_2(-j+N) - y_2(-N+N)*waDE(-j*h) - y_2( N+N)*wbDE(-j*h))
+                       * S(-j, h, DE_trans_inv(a, b, x));
+    }
+      ans += (y_2( 0+N) - y_2(-N+N)*waDE(0*h) - y_2( N+N)*wbDE(0*h))
+                       * S( 0, h, DE_trans_inv(a, b, x));
+      ans += y_2(-N+N)*wa(a, b, x) + y_2( N+N)*wb(a, b, x);
+
+    err = fabs(ex2_u(x) - ans);
+    //    std::cout << x << "\t" << err << std::endl;
+
+    if (maxerr < err)
+      maxerr = err;
+  }
+
+  end = clock();
+
+  time = (double)(end - start) / CLOCKS_PER_SEC;
+
+  std::cout << N << "\t" << time << "\t" << maxerr << std::endl;
+  }
+}
---
+++ DE_coll_example3.mak
@@ -0,0 +1,95 @@
+# Makefile ( n source file 1 execute file version )
+
+PACKAGE	= DE_coll_example3
+SRCS	= $(PACKAGE).cpp DE_trans.cpp
+HEADS	= DE_trans.h
+OBJS	= $(SRCS:.cpp=.o)
+FILES	= DE_coll_example3 $(HEADS) $(SRCS)
+VER	= `date +%Y%m%d`
+
+
+### command and flags ###
+# uncomment when debugging
+#DEBUG	= -ggdb -pg # -lefence
+
+# common (*.o)
+LD	= g++-4.0
+#CPPL_LD	= -L/sw/lib -llapack -lf77blas -latlas -lg2c -lm
+CPPL_LD	= -L/sw/lib -L/sw/lib/lapack/3.4.2 -lreflapack -lrefblas -lm
+GSL_LD	= `/sw/bin/gsl-config --libs`
+#-L/usr/lib/blas/atlas -lcblas  -L/usr/lib/lapack/atlas -llapack 
+LDFLAGS	= -g -Wall -Wno-unknown-pragmas $(DEBUG) $(CPPL_LD) $(GSL_LD)
+#LDLIBS	= -lm
+
+# C (*.c)
+CC	= gcc
+OPTIMIZE= -O2
+CFLAGS	= -g $(OPTIMIZE) -Wall $(DEBUG)
+CPPFLAGS= -I.
+
+# C++ (*.cpp)
+CXX	= g++-4.0
+GSL_FL	= `/sw/bin/gsl-config --cflags`
+CPPL_FL	= -I/sw/include -I/sw/include/cpplapack -Wno-unknown-pragmas
+CXXFLAGS= -g $(OPTIMIZE) -Wall $(DEBUG) $(CPPL_FL) $(GSL_FL) 
+
+# Fortran77 (*.f)
+FC	= f77
+FFLAGS	= -Wall $(DEBUG)
+
+# Pascal (*.p)
+PC	= pc
+PFLAGS	= -Wall $(DEBUG)
+
+# etc
+SHELL	= /bin/sh
+RM	= rm -f
+PROF	= gprof
+
+
+### rules ###
+
+.SUFFIXES:
+.SUFFIXES: .o .c .cpp .f .p
+
+all: $(PACKAGE)
+
+$(PACKAGE): $(OBJS)
+	$(LD) $(OBJS) $(LDFLAGS) -o $@ $(LDLIBS)
+
+$(OBJS): $(HEADS) $(PACKAGE).mak
+
+.c.o:
+	$(CC) $(CFLAGS) $(CPPFLAGS) -c $< -o $@
+.cpp.o:
+	$(CXX) $(CXXFLAGS) $(CPPFLAGS) -c $< -o $@
+.f.o:
+	$(FC) $(FFLAGS) -c $< -o $@
+.p.o:
+	$(PC) $(PFLAGS) $(CPPFLAGS) -c $< -o $@
+
+
+### useful commands ###
+
+clean:
+	$(RM) $(PACKAGE) $(OBJS)
+	$(RM) core gmon.out *~ #*#
+
+tar:
+	@echo $(PACKAGE)-$(VER) > .package
+	@$(RM) -r `cat .package`
+	@mkdir `cat .package`
+	@ln $(FILES) `cat .package`
+	tar cvf - `cat .package` | gzip -9 > `cat .package`.tar.gz
+	@$(RM) -r `cat .package` .package
+
+zip:
+	zip -9 $(PACKAGE)-$(VER).zip $(FILES)
+
+
+prof: run
+	$(PROF) $(PACKAGE) | less
+
+run: all
+#	./$(PACKAGE) < sample-data | less
+	./$(PACKAGE)
